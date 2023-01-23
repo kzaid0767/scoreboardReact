ced51c2f625cc4f3f919b55ceb8837fdc11de7b5
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2023-01-23 13:52:45 -0500

    time working

--- src/Clock.js
+++ src/Clock.js
@@ -1,25 +1,58 @@
-import React,{useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from './Button'
+/* import TimeInput from 'react-input-time' */
 
 function Clock() {
 
-    const [time, setTime] = useState(5)
+    const [time, setTime] = useState(30)
+    const [isTimeRuning, setIsTimeRunning] = useState(false)
 
     const handleChange = (e) => {
-        setTime(e.target.value)
+        setInterval(setTime(e.target.value),1000)
     }
 
+    const start = () => {
+        setIsTimeRunning(true)
+    }
 
-  return (
-    <div>
-        <input type={'number'} format={'mm:ss'} onChange={handleChange} value={time} />
+    const pause = () => {
+        setIsTimeRunning(false)
+    }
+
+    const reset = () => {
+        setIsTimeRunning(false)
+        setTime(30)
+    }
+
+    useEffect(
+        () => {
+            const timer = () => {
+                if (isTimeRuning && time>0) {
+                    setTime((time - 0.1).toFixed(1))
+                }
+            }
+
+            // if you want it to finish at some point
+            if (time === 0) {
+                return;
+            }
+            const id = setInterval(timer, 100)
+            return () => clearInterval(id)
+        },
+        [time, isTimeRuning]
+    )
+
+
+    return (
         <div>
-            <Button number={'start'} />
-            <Button number={'stop'} />
-            <Button number={'reset'} />
+            <input type={'number'} disabled={isTimeRuning} onChange={handleChange} value={time} />
+            <div>
+                <Button number={'start'} click={start} />
+                <Button number={'pause'} click={pause} />
+                <Button number={'reset'} click={reset} />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Clock
